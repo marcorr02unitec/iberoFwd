cfd272e22532e2e90fe0fe27eef46e0d73c96825
marcorr02unitec <43156434+marcorr02unitec@users.noreply.github.com>
2021-04-28 16:15:02 -0500

    Update app.controller.ts
    esto es una prueba

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+//prueba
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
